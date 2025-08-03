8d9970af47ffb3dc1358cb59e68db7ddc88887a6
Volodymyr Serdiukov <volodymyr_serdiukov@epam.com>
2025-08-03 18:56:03 +0300

    ADO-4 Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-adolearn-vserdiukov-tfstate"
-    storage_account_name = "stadovserdiukovtfstate"
-    container_name       = "tfstate"
-    key                  = "validation.tfstate"
-    use_msi              = false
-  }
-}
+
