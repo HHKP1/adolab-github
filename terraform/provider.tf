7edbf4dbfb0fd1cd6a7f468e12d210b59781043d
Volodymyr Serdiukov <volodymyr_serdiukov@epam.com>
2025-08-04 21:05:16 +0000

    ADO-5 Update provider version
    Update provider version
    
    Related work items: #22

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,7 +5,7 @@ terraform {
       version = ">=3.0"
     }
   }
-  required_version = ">= 1.0.4"
+  required_version = ">= 1.0"
 
   backend "azurerm" {
     # Configuration will be provided via pipeline
